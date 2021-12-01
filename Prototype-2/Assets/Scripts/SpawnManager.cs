dbac7f7dcc6431415c0412a0cecfe167a478f2fa
Douglas Urner <dlu@canishe.com>
2021-12-01 14:50:26 -0800

    2.3.4: Randomize the spawn location

--- Prototype-2/Assets/Scripts/SpawnManager.cs
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,9 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            // Select a random animal and a location to spawn them at.
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
         }
     }
